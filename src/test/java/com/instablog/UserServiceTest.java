0de71db9959d3b35acad9423b14c6bbcb594113d
pradedes <pradeep.deshmukhmane@gmail.com>
2017-05-25 22:57:26 +0530

    test case

---
+++ src/test/java/com/instablog/UserServiceTest.java
@@ -0,0 +1,21 @@
+package com.instablog;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class UserServiceTest extends TestCase {
+
+
+	@Test
+	public void testadd() {
+		//test User add
+	
+	}
+	
+	@Test
+	public void testSignup() {
+		//test User signup
+	}
+
+}
