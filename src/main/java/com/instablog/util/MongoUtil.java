e53b87ed3d0480ed628af0edb4f56520b66ca62b
pradedes <pradeep.deshmukhmane@gmail.com>
2017-06-23 10:17:14 +0530

    mongo db ip

--- src/main/java/com/instablog/util/MongoUtil.java
+++ src/main/java/com/instablog/util/MongoUtil.java
@@ -8,9 +8,9 @@ import com.mongodb.MongoClient;
 public class MongoUtil {
 	
 	public static Datastore initMongo(){
-		System.out.println("MongoUtil.initMongo()************************************");
+		System.out.println("MongoUtil.initMongo() test sfgf************************************");
 		//MongoClient mongoClient = new MongoClient(":27017");
-		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		MongoClient mongoClient = new MongoClient("35.188.58.122", 27017);
 		Morphia morphia = new Morphia();
 		String databaseName = "instablog";
 		Datastore datastore = morphia.createDatastore(mongoClient, databaseName);
